e7d986188100848d78697772d52ab8a4526171eb
Sheiwa <lengyel.noemi96@gmail.com>
2017-08-10 13:39:54 +0200

    Delete method added

--- Yetic_MeLo/Yetic_MeLo/DatabaseManager.cs
+++ Yetic_MeLo/Yetic_MeLo/DatabaseManager.cs
@@ -19,15 +19,24 @@ namespace Yetic_MeLo
             connection.Close();
         }
 
+        // Folders Table
         void AddNewRowToFolderTable(string path, bool isVideo, bool isPicture, bool isMusic)
         {
-            string query = "INSERT INTO Folders VALUES(" + path + "," + isVideo + "," + isPicture + "," + isMusic + ")";
+            string query = "INSERT INTO Folders VALUES('" + path + "','" + isVideo + "','" + isPicture + "','" + isMusic + "')";
             Execute(query);
         }
 
+        // Settings Table
         void AddNewRowToSettingsTable(string category, string extension, bool check)
         {
-            string query = "INSERT INTO Settings VALUES(" + category + "," + extension + "," + check + ")";            
+            string query = "INSERT INTO Settings VALUES('" + category + "','" + extension + "','" + check + "')";            
+            Execute(query);
+        }
+
+        // Delete Row
+        void DeleteRowFromTable(string table, int id)
+        {
+            string query = "DELETE FROM" + table + "WHERE Id ='" + id.ToString() + "';";
             Execute(query);
         }
     }    
