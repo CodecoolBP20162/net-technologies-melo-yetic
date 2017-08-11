bb9680fc5ec342223adebe236aee543cce849f03
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-08-11 19:45:42 +0200

    Added basic logic for scanning folder content and watching new files.

---
+++ Yetic - MeLo/Yetic - MeLo/FolderWatcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSelector
+{
+    class FolderWatcher
+    {
+        FileSystemWatcher watcher;
+        public List<string> AllowedExtensions { get; set; }
+
+        public FolderWatcher() { }
+
+        public void WatchFolder(string Path, List<string> AllowedExtensions)
+        {
+            watcher = new FileSystemWatcher();
+            this.AllowedExtensions = AllowedExtensions;
+            watcher.Path = Path;
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                         | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true;
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            if (AllowedExtensions.Contains(Path.GetExtension(e.FullPath).ToLower()))
+            {
+                Console.WriteLine("File created:");
+                Console.WriteLine(e.FullPath);
+            }
+
+        }
+    }
+}
---
+++ Yetic_MeLo/Yetic_MeLo/FolderScanner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSelector
+{
+    class FolderScanner
+    {
+        public List<string> FoundFileList { get; set; }
+
+
+        public FolderScanner()
+        {
+            FoundFileList = new List<string>();
+        }
+
+        public List<string> GetFiles(string Destination, List<string> AllowedExtensions)
+        {
+            try
+            {
+                var files = Directory.GetFiles(Destination, "*.*", SearchOption.AllDirectories)
+                                .Where(s => AllowedExtensions.Contains(Path.GetExtension(s).ToLower()));
+                FoundFileList.AddRange(new List<string>(files));
+                return FoundFileList;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public List<string> GetFiles(List<string> FolderList, List<string> AllowedExtensions)
+        {
+
+            foreach (string Destination in FolderList)
+            {
+                GetFiles(Destination, AllowedExtensions);
+            }
+
+            return FoundFileList;
+        }
+    }
+}
