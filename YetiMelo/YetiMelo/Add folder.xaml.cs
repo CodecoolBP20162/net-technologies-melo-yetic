27615c8130b2960d3bc0a4dfb9e8de15bcc4798a
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-08-16 15:49:31 +0200

    Started Merging UI and Media Player, folder scanning, and file system watching (WIP)

---
+++ YetiMelo/YetiMelo/Add folder.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+
+namespace YetiMelo
+{
+    /// <summary>
+    /// Interaction logic for Add_folder.xaml
+    /// </summary>
+    public partial class Add_folder : Window
+    {
+        public bool save;
+        public string newPath;
+
+        public Add_folder()
+        {
+            InitializeComponent();
+        }
+
+        private void btAddFolder_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.save = true;
+
+            //write the Query here!!
+        }
+
+        private void btClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(this.save))
+            {
+                MessageBoxResult result = System.Windows.MessageBox.Show("Do you want to save changes?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    btAddFolder_Click(sender, e);
+                }
+                this.Close();
+            }
+            this.Close();
+
+        }
+
+        private void btSelect_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    tbPath.Text = fbd.SelectedPath;
+                    this.newPath = fbd.SelectedPath;
+                }
+            }
+
+        }
+    }
+}
---
+++ YetiMelo/YetiMelo/MergeMp3.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YetiMelo
+{
+    /// <summary>
+    /// Interaction logic for MergeMp3.xaml
+    /// </summary>
+    public partial class MergeMp3 : Window
+    {
+        public MergeMp3()
+        {
+            InitializeComponent();
+        }
+
+        private void btClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btSelectDesitnation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btSelectDesitnation_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btSelectMp32_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btSelectMp3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btMerge_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
